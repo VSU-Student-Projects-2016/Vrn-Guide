487bd239c7184421278ef1ebeaf34c275494cfbd
YellowfinBird <kioyami@mail.ru>
2016-10-15 01:21:40 +0300

    add map with point

--- VRN_Guide/ViewController.swift
+++ VRN_Guide/ViewController.swift
@@ -7,18 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var MyMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let currLocation=CLLocation(latitude: 43.7229843, longitude: 10.3966227)
+        centerMapOnLocation(location: currLocation)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        MyMap.setRegion(coordinateRegion, animated: true)
     }
+
+  
+    
+    
+    
+    
 
 
 }

c61b76ee73e5d5b6e441d1150328fa065893c53d
YellowfinBird <kioyami@mail.ru>
2016-10-22 14:17:50 +0300

    bridging header added

---
+++ VRN_Guide-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  VRN_Guide-Bridging-Header.h
+//  VRN_Guide
+//
+//  Created by Надежда on 22.10.16.
+//  Copyright © 2016 PTeam. All rights reserved.
+//
+
+#ifndef VRN_Guide_Bridging_Header_h
+#define VRN_Guide_Bridging_Header_h
+
+
+#endif /* VRN_Guide_Bridging_Header_h */

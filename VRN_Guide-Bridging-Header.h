6ea17ccd6ee5573093d3a5e68e2078985010960d
YellowfinBird <kioyami@mail.ru>
2016-10-29 12:00:53 +0300

    custom cell class added

--- VRN_Guide-Bridging-Header.h
+++ VRN_Guide-Bridging-Header.h
@@ -8,6 +8,6 @@
 
 #ifndef VRN_Guide_Bridging_Header_h
 #define VRN_Guide_Bridging_Header_h
-
+#import <CCHMapClusterController/CCHMapClusterController.h>
 
 #endif /* VRN_Guide_Bridging_Header_h */
---
+++ VRN_Guide/MyCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCell.swift
+//  VRN_Guide
+//
+//  Created by Надежда on 22.10.16.
+//  Copyright © 2016 PTeam. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

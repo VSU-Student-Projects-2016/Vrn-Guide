7d5b68c0fa41751d3fa1b166d8ab29fa72a60f94
YellowfinBird <kioyami@mail.ru>
2016-11-26 10:26:11 +0300

    Realm added

--- Podfile
+++ Podfile
@@ -4,8 +4,9 @@
 target 'VRN_Guide' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
-	pod ‘CCHMapClusterController’
+	pod 'CCHMapClusterController'
 	pod 'JPSThumbnailAnnotation'
+  pod 'RealmSwift'
 
   # Pods for VRN_Guide
 
--- Pods/Target Support Files/Pods-VRN_Guide/Pods-VRN_Guide-frameworks.sh
+++ Pods/Target Support Files/Pods-VRN_Guide/Pods-VRN_Guide-frameworks.sh
@@ -86,8 +86,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/CCHMapClusterController/CCHMapClusterController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JPSThumbnailAnnotation/JPSThumbnailAnnotation.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/CCHMapClusterController/CCHMapClusterController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JPSThumbnailAnnotation/JPSThumbnailAnnotation.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
 fi

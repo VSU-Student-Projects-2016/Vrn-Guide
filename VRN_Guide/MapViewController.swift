084cbd363f8d3c30eaf39d5498ec75a4ff851009
MIDERY <MIDERY@miderys-mac.local>
2016-12-10 16:43:10 +0300

    realm

--- VRN_Guide/MapViewController.swift
+++ VRN_Guide/MapViewController.swift
@@ -11,12 +11,15 @@ import UIKit
 import MapKit
 import CCHMapClusterController
 import JPSThumbnailAnnotation
+import RealmSwift
 
 class MapViewController: UIViewController, CCHMapClusterControllerDelegate, MKMapViewDelegate {
 
   
     @IBOutlet weak var MyMap: MKMapView!
-
+    var item_realm = try! Realm()
+    var array_of_artwork = [MKPointAnnotation]()
+    
     var p: CCHMapClusterController = CCHMapClusterController.init()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,16 +27,26 @@ class MapViewController: UIViewController, CCHMapClusterControllerDelegate, MKMa
         self.p = CCHMapClusterController.init(mapView: self.MyMap)
         self.p.delegate = self
         self.MyMap.delegate = self
+        loadRealm()
+        
+        /*let startLocation = CLLocationCoordinate2D(latitude: 51.675611, longitude: 39.211179)
+        let sl = CLLocationCoordinate2D (latitude:51.674773, longitude: 39.209151)*/
+        StartLocation()
+        
+        let arr = item_realm.objects(SingleItem.self)
+        for index in arr{
+            let artan = MKPointAnnotation()
+            let cord = CLLocationCoordinate2D(latitude: index.item_latitude, longitude: index.item_longitude)
+            artan.coordinate = cord
+            artan.title = index.name
+            artan.subtitle = index.context
+            array_of_artwork.append(artan)
+        }
         
         
-        let startLocation = CLLocationCoordinate2D(latitude: 51.675611, longitude: 39.211179)
-        let sl = CLLocationCoordinate2D (latitude:51.674773, longitude: 39.209151)
-        let span = MKCoordinateSpanMake(0.3, 0.3)
-        let region = MKCoordinateRegion(center: sl, span: span)
+        p.addAnnotations(array_of_artwork, withCompletionHandler: nil)
         
-        MyMap.setRegion(region, animated: true)
-        
-        let dropPin = MKPointAnnotation()
+        /*let dropPin = MKPointAnnotation()
         dropPin.coordinate = sl
         dropPin.title = "New York City"
         //MyMap.addAnnotation(dropPin)
@@ -55,23 +68,54 @@ class MapViewController: UIViewController, CCHMapClusterControllerDelegate, MKMa
     
         
         //MyMap.addAnnotation(JPSThumbnailAnnotation(thumbnail: fr))
-        p.addAnnotations([JPSThumbnailAnnotation(thumbnail: fr)], withCompletionHandler: nil)
+        p.addAnnotations([JPSThumbnailAnnotation(thumbnail: fr)], withCompletionHandler: nil) */
         
        
     }
     
-    
+    //отцентровка
+    func StartLocation(){
+        let sl = CLLocationCoordinate2D (latitude:51.674773, longitude: 39.209151)
+        let span = MKCoordinateSpanMake(0.3, 0.3)
+        let region = MKCoordinateRegion(center: sl, span: span)
+        
+        MyMap.setRegion(region, animated: true)
+    }
     
     func LoadPoint(start: CLLocation){
         let Radius:CLLocationDistance = 2000
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(start.coordinate, Radius, Radius)
         MyMap.setRegion(coordinateRegion, animated: true)
-        
-        
-
     }
     
-    
+    //pfuheprf htfkvf
+    func loadRealm()
+    {
+        let item_1=SingleItem()
+        item_1.image="cat1"
+        item_1.item_id=0
+        item_1.name="cat 1"
+        item_1.context="cute cat"
+        item_1.item_description="there will be a description Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda. Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
+        item_1.item_latitude=51.674773
+        item_1.item_longitude=39.209151
+        
+        let item_2=SingleItem()
+        item_2.image="cat2"
+        item_2.item_id=0
+        item_2.name="cat 2"
+        item_2.context="cute cat"
+        item_2.item_description="there will be a description Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda. Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
+        item_2.item_latitude=51.675611
+        item_2.item_longitude=39.211179
+        
+        try! item_realm.write {
+            item_realm.deleteAll()
+            item_realm.add(item_1)
+            item_realm.add(item_2)
+        }
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -109,14 +153,18 @@ class MapViewController: UIViewController, CCHMapClusterControllerDelegate, MKMa
             return String(x)
             
         }
-        let sl = CLLocationCoordinate2D (latitude:51.674773, longitude: 39.209151)
-        if mapClusterAnnotation.coordinate.latitude == sl.latitude {
-            return String(mapClusterAnnotation.coordinate.latitude)
-        }
-        else{
-            return "2 метка"
+        return ""
         }
         
+        return ""
+//        if (mapClusterAnnotation.index == 0)
+//        array_of_artwork
+//            return String()
+//        }
+//        else{
+//            return "2 метка"
+//        }
+//        item_realm.objects(SingleItem.self)[idx]
     }
     
     
